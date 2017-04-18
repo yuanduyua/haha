258def1645fddbd07a7ab7eac42cb905e6cca443
yuan <615863500@qq.com>
2017-04-18 09:19:47 +0800

    6次提交

--- src/main/java/com/example/dutest/MainActivity.java
+++ src/main/java/com/example/dutest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String i="今天的学习成果";
+        String i="234";
     }
 }
